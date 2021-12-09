2bfc12172dd081df35b52a5f5c3073fdf3bd6cde
huuthiendev <huuthiendev@gmail.com>
2021-12-09 19:03:53 +0700

    update home page style

--- assets/js/HomeController.js
+++ assets/js/HomeController.js
@@ -1,3 +1,21 @@
 angular.module('42RaceMini', []).controller('HomeController', function ($scope, $http) {
-  
+  $scope.accountInfo = null;
+
+  angular.element(document).ready(function () {
+    $scope.getAccountInfo();
+  });
+
+  $scope.getAccountInfo = function () {
+    $http({
+      method: 'GET',
+      url: '/account/info'
+    }).then(function (res) {
+      $scope.accountInfo = res.data;
+    });
+  }
+
+  $scope.getAthleteURL = function () {
+    if (!$scope.accountInfo) return '';
+    return 'https://strava.com/athletes/' + $scope.accountInfo.athleteId;
+  }
 });
